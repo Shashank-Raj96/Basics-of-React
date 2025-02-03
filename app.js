0c1fe880ba60465b491230c75e714c90be1a3884
Shashank-Raj96 <shashankraj0915@gmail.com>
2025-02-04 00:36:48 +0530

    Printing Hello World using React

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * <div id="parent">
+ *       <div id="child">
+ *          <h1> I am Shashank </h1>
+ *       </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+    "div", 
+    { id:"parent"},
+React.createElement(
+    "div",
+    { id:"child"},
+    React.createElement("h1", {},"I am Shashank Raj")
+)
+);
+
+//const heading = React.createElement("h1",{id: "heading"},"Hello World From React");
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
